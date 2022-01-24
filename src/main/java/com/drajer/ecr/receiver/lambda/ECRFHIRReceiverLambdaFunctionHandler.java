8dec8967cf939181744ceb637247633ce2c4ceba
Arun Kumar <arunkumarhk@gmail.com>
2022-01-23 21:50:49 -0500

    Added token Handler

--- src/main/java/com/drajer/ecr/receiver/lambda/ECRFHIRReceiverLambdaFunctionHandler.java
+++ src/main/java/com/drajer/ecr/receiver/lambda/ECRFHIRReceiverLambdaFunctionHandler.java
@@ -1,14 +1,9 @@
 package com.drajer.ecr.receiver.lambda;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.nio.charset.Charset;
+import java.util.Map;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -18,44 +13,44 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.amazonaws.util.StringUtils;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-public class ECRFHIRReceiverLambdaFunctionHandler implements RequestStreamHandler {
+public class ECRFHIRReceiverLambdaFunctionHandler
+		implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
 	@Override
-	public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
 
 		LambdaLogger logger = context.getLogger();
+		APIGatewayProxyResponseEvent apiGatewayProxyResponseEvent = new APIGatewayProxyResponseEvent();
 
-		// URL where the request will be forwarded 
+		// URL where the request will be forwarded
 		String httpPostUrl = System.getenv("HTTP_POST_URL");
 
-		if (StringUtils.isNullOrEmpty(httpPostUrl)) {
+		if (httpPostUrl == null) {
 			throw new RuntimeException("HTTP_POST_URL Environment variable not configured");
 		}
 		logger.log("HTTP Post URL " + httpPostUrl);
 
-		// Read the input stream
-		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
-		PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream, Charset.forName("UTF-8"))));
-
 		try {
 
-			StringBuilder inputStrBuilder = new StringBuilder();
-			String inputStr;
-			while ((inputStr = reader.readLine()) != null)
-				inputStrBuilder.append(inputStr);
+			Map<String, String> headers = request.getHeaders();
+			String authHeader = headers.get("Authorization");
+
+			logger.log("Token: " + authHeader);
 
 			// Create a instance of httpClient and forward the request
 			DefaultHttpClient httpClient = new DefaultHttpClient();
 
-			// Add content type as application / json 
+			// Add content type as application / json
 			HttpPost postRequest = new HttpPost(httpPostUrl);
 			postRequest.addHeader("accept", "application/json");
-			StringEntity input = new StringEntity(inputStrBuilder.toString());
+			StringEntity input = new StringEntity(request.getBody());
 			input.setContentType("application/json");
 			postRequest.setEntity(input);
+			postRequest.addHeader("Authorization", authHeader);
 
 			logger.log("Forwarding the request to FHIR Validator ");
 
@@ -63,7 +58,7 @@ public class ECRFHIRReceiverLambdaFunctionHandler implements RequestStreamHandle
 
 			HttpResponse response = httpClient.execute(postRequest);
 
-			// Check return status and throw Runtime exception for return code != 200 
+			// Check return status and throw Runtime exception for return code != 200
 			if (response.getStatusLine().getStatusCode() != 200) {
 				throw new RuntimeException("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
 			}
@@ -72,11 +67,11 @@ public class ECRFHIRReceiverLambdaFunctionHandler implements RequestStreamHandle
 			String output;
 			StringBuilder outputStr = new StringBuilder();
 			logger.log("Response from FHIR Validator .... ");
-			// Write the response back to invoking program 
+			// Write the response back to invoking program
 			while ((output = br.readLine()) != null) {
 				outputStr.append(output);
 			}
-			writer.write(outputStr.toString());
+			apiGatewayProxyResponseEvent.setBody(outputStr.toString());
 			logger.log(outputStr.toString());
 			br.close();
 			httpClient.getConnectionManager().shutdown();
@@ -84,11 +79,12 @@ public class ECRFHIRReceiverLambdaFunctionHandler implements RequestStreamHandle
 		} catch (ClientProtocolException e) {
 			throw new RuntimeException("Failed with ClientProtocolException: " + e.getMessage());
 		} catch (IOException e) {
-			throw new RuntimeException("Failed with IOException: "+e.getMessage());
+			throw new RuntimeException("Failed with IOException: " + e.getMessage());
 		} finally {
-			reader.close();
-			writer.close();
+//			reader.close();
+//			writer.close();
 		}
+		return apiGatewayProxyResponseEvent;
 
 	}
 
